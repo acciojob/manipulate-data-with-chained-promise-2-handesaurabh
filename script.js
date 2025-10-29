f1423ec6c09262ad3a880e30513ed6ea424ae206
handesaurabh <handesaurabh2004@gmail.com>
2025-10-29 11:48:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 //your JS code here. If required.
-const numbers=[1,2,3,4];
-const output=document.getelementById("output");
-const promise=new Promise((resolve)=>{
-	setTimeout(()=>{
-		resolve(numbers);
-	},3000)
-})
-numbers
-.then((arr)=>{
-	return new Promise((resolve)=>{
-		const evenNum=arr.filter((num)=>num%2===0);
-		output.innerText=evenNum.join(",");
-		resolve(evenNum);
-	},1000)
-})
-.then((even)=>{
-	return new Promise((resolve)=>{
-		setTimeout(()=>{
-			const multiply=even.map((num)=>num*2);
-			output.innerText=multiply.join(",");
-			resolve(multiply);
-		},2000)
-	})
-})
-.then((result)=>console.log("Result: ",result));
+const numbers = [1, 2, 3, 4];
+const output = document.getElementById("output");
+const promise = new Promise(resolve => {
+  setTimeout(() => {
+    resolve(numbers);
+  }, 3000);
+});
+
+promise
+  .then(arr => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        const evenNum = arr.filter(num => num % 2 === 0);
+        output.innerText = evenNum.join(",");
+        resolve(evenNum);
+      }, 1000);
+    });
+  })
+  .then(even => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        const multiply = even.map(num => num * 2);
+        output.innerText = multiply.join(",");
+        resolve(multiply);
+      }, 2000);
+    });
+  })
+  .then(result => {
+    console.log("Result:", result);
+  });
